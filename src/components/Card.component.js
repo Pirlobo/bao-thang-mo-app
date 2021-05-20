392beceb36ea249304e5cb91574de59ff796f985
Pirlobo <bonguyens2001@gmail.com>
2021-05-20 03:56:52 -0700

    first commit

--- src/components/Card.component.js
+++ src/components/Card.component.js
@@ -2,15 +2,21 @@ import React from "react";
 import history from "../History";
 const Card = (props) => {
   const handleClick = (e) => {
+    
     if (e.target.value == 0) {
+      console.log(e.target.value);
       history.push("/tim-kiem");
     } else if (e.target.value == 1) {
+      console.log(e.target.value);
       history.push("/dang-viec-lam");
     } else if (e.target.value == 2) {
+      console.log(e.target.value);
       history.push("/cho-thue-phong");
     } else if (e.target.value == 3) {
+      console.log(e.target.value);
       history.push("/ban-xe");
     }
+    
   };
   return (
     <div className="card">
@@ -21,7 +27,7 @@ const Card = (props) => {
         value={props.value}
         className="btn btn-get-started"
       >
-        <a>Get Started</a>
+        Get Started
       </button>
     </div>
   );
